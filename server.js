b83d76220a94f82a16fd138bb792edab35ddce15
fr <fazlerabby122@gmail.com>
2024-09-17 12:38:25 +0600

    request header fix

--- server.js
+++ server.js
@@ -32,21 +32,29 @@ const port = process.env.PORT || 3000;
 connectDB();
 const app = express();
 
-// app.use(
-//     cors({
-//         origin: [
-//             "http://localhost:5173",      
-//         ],
-//         methods:['GET','POST','PUT','DELETE'],
-//         credentials: true,
-//         // allowedHeaders: ["Content-Type", "Authorization"],
-//     })
-// );
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",      
+        ],
+        methods:['GET','POST','PUT','DELETE'],
+        credentials: true,
+        // allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
-app.use('*', cors());
+// app.use('*', cors());
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
 
 
 
