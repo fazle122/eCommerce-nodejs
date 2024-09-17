551b3b553697cb3b1542a7b6ede66b329db852b8
fr <fazlerabby122@gmail.com>
2024-09-17 12:33:59 +0600

    cors fix 1

--- server.js
+++ server.js
@@ -32,19 +32,18 @@ const port = process.env.PORT || 3000;
 connectDB();
 const app = express();
 
-app.use(
-    cors({
-        origin: [
-            "http://localhost:5173",
-            "https://e-ccommerce-nodejs.vercel.app"
-        ],
-        methods:['GET','POST','PUT','DELETE'],
-        credentials: true,
-        // allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+// app.use(
+//     cors({
+//         origin: [
+//             "http://localhost:5173",      
+//         ],
+//         methods:['GET','POST','PUT','DELETE'],
+//         credentials: true,
+//         // allowedHeaders: ["Content-Type", "Authorization"],
+//     })
+// );
 
-// app.use('*', cors());
+app.use('*', cors());
 app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
